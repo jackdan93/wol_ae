2c2588676ec7c20f72e01566ef4a94f4f22dfcf7
Daniele Saulle <jackdan93@virgilio.it>
2021-11-26 10:15:32 +0100

    WakeOnLAN And Exit first complete commit (working)

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace WOL_AE
+{
+    [Command(Name = "WOL_AE.exe", Description = "Command line tool which sends WOL magic packet to the specified destination and quit")]
+    class Program
+    {
+        [Argument(0, "mac", "The MAC address to wake up")]
+        [Required]
+        public string MacAddress { get; }
+
+        [Option("-ip|--ipAddress", Description = "Optional: the IP address to which send the WOL packets (255.255.255.255 by default)")]
+        public string IPAddress { get; }
+        
+        [Option("-p|--port", Description = "Optional: the port to be used when sending WOL packets (9 by default)")]
+        public int? PortNumber { get; }
+
+        [Option("-v|--verbose", Description = "Enable verbose logging")]
+        public bool Verbose { get; }
+
+        private void OnExecute()
+        {
+            Console.WriteLine("Starting WOL_AE application with the following arguments:");
+            Console.WriteLine($"MacAddress: {MacAddress}");
+            Console.WriteLine($"IPAddress: {IPAddress}");
+            Console.WriteLine($"PortNumber: {PortNumber}");
+
+            using (WOLClient cl = new WOLClient())
+            {
+                Console.WriteLine(cl.SendWOL(Verbose, MacAddress, IPAddress, PortNumber)
+                                  ? "WOL correctly sent"
+                                  : "WOL failed");
+            }
+        }
+
+        public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+    }
+}
---
+++ WOLClient.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WOL_AE
+{
+    public class WOLClient : UdpClient
+    {
+        private const string DEFAULT_BROADCAST_ADDRESS = "255.255.255.255";
+        private const int DEFAULT_PORT = 9;
+
+        public WOLClient() : base() { }
+
+        /// <summary>
+        /// Send magic packets to wake up the specified MAC_ADDRESS
+        /// </summary>
+        /// <param name="MAC_ADDRESS">MAC address to wake up</param>
+        /// <param name="ipAddr">Optional: the IP address to which send the WOL packets (255.255.255.255 by default)</param>
+        /// <param name="portNum">Optional: the port to be used when sending WOL packets (9 by default)</param>
+        /// <returns></returns>
+        internal bool SendWOL(bool Verbose, string MAC_ADDRESS, string ipAddr = null, int? portNum = null)
+        {
+            try
+            {
+                //Parse and validate arguments
+                if (Verbose) Console.WriteLine($"Validating MAC address {MAC_ADDRESS}");
+                string validatedMACAddress = ParseAndValidateMacAddress(MAC_ADDRESS);
+                if (Verbose) Console.WriteLine($"Validated MAC address: {validatedMACAddress}");
+
+                if (Verbose) Console.WriteLine($"Validating IP address {ipAddr}");
+                IPAddress validatedIPAddress = ParseAndValidateIPAddress(ipAddr);
+                if (Verbose) Console.WriteLine($"Validated IP address: {validatedIPAddress}");
+
+                if (Verbose) Console.WriteLine($"Validating port number {portNum}");
+                int validatedPortNumber = ParseAndValidatePortNumber(portNum);
+                if (Verbose) Console.WriteLine($"Validated port number: {validatedPortNumber}");
+
+                if (Verbose) Console.WriteLine("Connecting the UDP client to the specified host");
+                //Connect the UDP client
+                this.Connect(validatedIPAddress, validatedPortNumber);
+
+                if (Verbose) Console.WriteLine("Setting UDP client socket options");
+                //Set UDP client socket options (or throw exception if it's still not connected)
+                if (!this.Active)
+                    throw new WOLClientException("UDP client cannot connect to remote host, aborting...");
+                this.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 0);
+
+                //Buffer to be sent (size is: 6 reserved bytes + MAC bytes length * number of repeat)
+                int byteSize = 6 + (6 * 16);
+                byte[] bytes = new byte[byteSize];
+                int setBytesCounter = 0;
+
+                if (Verbose) Console.WriteLine("Reserving first 6 bytes of magic packet");
+                //First 6 bytes must be 0xFF
+                for (int y = 0; y < 6; y++)
+                    bytes[setBytesCounter++] = 0xFF;
+
+                if (Verbose) Console.WriteLine("Converting 16 copies of MAC address as hexadecimal numbers");
+                //Repeat MAC 16 times
+                for (int y = 0; y < 16; y++)
+                {
+                    int i = 0;
+                    for (int z = 0; z < 6; z++)
+                    {
+                        bytes[setBytesCounter++] = byte.Parse(validatedMACAddress.Substring(i, 2), NumberStyles.HexNumber);
+                        i += 2;
+                    }
+                }
+
+                if (Verbose) Console.WriteLine("Sending magic packets to the network");
+                //Send wake up packet
+                int bytesSent = Send(bytes, byteSize);
+                if (Verbose) Console.WriteLine($"Bytes size: {byteSize} | Sent bytes: {bytesSent}");
+                return (bytesSent == byteSize);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine((ex is WOLClientException) ? ex.Message : writeWellFormedException("Unexpected error detected while sending magic packets", ex));
+                return false;
+            }
+        }
+
+        private string ParseAndValidateMacAddress(string MACAddr) 
+        {
+            //Null check
+            if (string.IsNullOrWhiteSpace(MACAddr))
+                throw new WOLClientException("MAC ADDRESS not defined! Aborting...");
+
+            //Sanitize MAC_ADDRESS: remove any character which is not in [A-Z0-9]
+            if (MACAddr.Any(character => !(char.IsDigit(character) || char.IsLetter(character))))
+                MACAddr = string.Join("", MACAddr.Where(character => char.IsDigit(character) || char.IsLetter(character))).Trim();
+
+            //Length check
+            if (MACAddr.Length != 12)
+                throw new WOLClientException("MAC ADDRESS length is not standard (12 digits --> 6 bytes)! Aborting...");
+
+            return MACAddr;
+        }
+
+        private IPAddress ParseAndValidateIPAddress(string ipAddrStr)
+        {
+            //Null check
+            if (string.IsNullOrWhiteSpace(ipAddrStr))
+            {
+                //Set default broadcast IP
+                ipAddrStr = DEFAULT_BROADCAST_ADDRESS;
+            }
+            else 
+            {
+                //Check format
+                if (ipAddrStr.Where(character => character == '.').Count() != 3 
+                    || ipAddrStr.Length < 7 || ipAddrStr.Length > 15)
+                    throw new WOLClientException("IP address format is not standard! Aborting...");
+            }
+
+            return IPAddress.Parse(ipAddrStr);
+        }
+
+        private int ParseAndValidatePortNumber(int? portNum)
+        {
+            //Null check
+            if (!portNum.HasValue)
+            {
+                //Set default port number
+                portNum = DEFAULT_PORT;
+            }
+            else
+            {
+                //Check format
+                if ((portNum != 7 || portNum != 9) && (portNum < 1024 || portNum > 65353))
+                    throw new WOLClientException("Invalid port number specified! Only 7, 9, or a value within the range 1024-65353 can be used! Aborting...");
+            }
+
+            return portNum.Value;
+        }
+
+        /// <summary>
+        /// Prepare well formed exception message
+        /// </summary>
+        /// <param name="message">Default error message</param>
+        /// <param name="ex">Exception object</param>
+        /// <returns></returns>
+        private string writeWellFormedException(string message, Exception ex)
+        {
+            message += Environment.NewLine + string.Format("Exception TargetSite: {0}", ex.TargetSite)
+                       + Environment.NewLine + string.Format("Exception Message: {0}", ex.Message)
+                       + Environment.NewLine + string.Format("Exception StackTrace: {0}", ex.StackTrace);
+            if (ex.InnerException != null)
+            {
+                message += Environment.NewLine + string.Format("Inner exception TargetSite: {0}", ex.InnerException.TargetSite)
+                       + Environment.NewLine + string.Format("Inner exception Message: {0}", ex.InnerException.Message)
+                       + Environment.NewLine + string.Format("Inner exception StackTrace: {0}", ex.InnerException.StackTrace);
+            }
+            return message;
+        }
+
+        /// <summary>
+        /// Exception handler
+        /// </summary>
+        internal class WOLClientException : Exception
+        {
+            public WOLClientException() : base() { }
+
+            public WOLClientException(string message) : base(message) { }
+        }
+    }
+}
